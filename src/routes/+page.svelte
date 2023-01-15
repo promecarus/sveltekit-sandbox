f2e6af8c27b21b4e31a5e9f03423aa4c051a1217
promecarus <haikalslipi@gmail.com>
2023-01-16 05:51:05 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
+<script>
+	let name = 'world';
+	const handleClick = () => (name = name === 'world' ? 'SvelteKit' : 'world');
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<h1 class="text-3xl font-bold underline">Hello {name}!</h1>
+<input type="text" bind:value={name} />
+<button on:click={handleClick}> Click me </button>
