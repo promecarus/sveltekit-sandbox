09f6c846723ddfb7368353e6e442b12ea87c3d98
promecarus <haikalslipi@gmail.com>
2023-01-20 22:28:57 +0700

    Add string to case converter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,32 @@
 <script>
-	let name = 'world';
-	const handleClick = () => (name = name === 'world' ? 'SvelteKit' : 'world');
+	let text = 'woRld of waRCraft';
+	$: camelCase = text
+		.toLowerCase()
+		.replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) =>
+			index === 0 ? word.toLowerCase() : word.toUpperCase()
+		)
+		.replace(/\s+/g, '');
+	$: kebabCase = text.toLowerCase().replace(/\s/g, '-');
+	$: lowerCase = text.toLowerCase();
+	$: pascalCase = text
+		.toLowerCase()
+		.replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => word.toUpperCase())
+		.replace(/\s+/g, '');
+	$: snakeCase = text.toLowerCase().replace(/\s/g, '_').toLowerCase();
+	$: titleCase = text
+		.toLowerCase()
+		.replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => word.toUpperCase())
+		.replace(/\s+/g, ' ');
+	$: upperCase = text.toUpperCase();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class="text-3xl font-bold underline">Hello {name}!</h1>
-<input type="text" bind:value={name} />
-<button on:click={handleClick}> Click me </button>
+<div class="text-center">
+	<input type="text" bind:value={text} class="text-center" />
+</div>
+<p>camelCase: {camelCase}</p>
+<p>kebab-case: {kebabCase}</p>
+<p>lower case: {lowerCase}</p>
+<p>PascalCase: {pascalCase}</p>
+<p>snake_case: {snakeCase}</p>
+<p>Title Case: {titleCase}</p>
+<p>UPPER CASE: {upperCase}</p>
